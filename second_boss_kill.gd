753cdb645958c6a5106602074725e6cf17d5bb2c
Pleseanu Cristian <ionut.pleseanu@stud.acs.upb.ro>
2025-04-06 13:40:03 +0300

    Final-Version 1.0 not even alfa (sigma)

--- second_boss_kill.gd
+++ second_boss_kill.gd
@@ -8,4 +8,4 @@ func _input(event):
 	if Input.is_action_just_pressed("continue"):
 		anim_player.play("Text")
 		await get_tree().create_timer(1.7).timeout
-		get_tree().change_scene_to_file("res://dungeon 2.tscn")
+		get_tree().change_scene_to_file("res://dunegon_3.tscn")
